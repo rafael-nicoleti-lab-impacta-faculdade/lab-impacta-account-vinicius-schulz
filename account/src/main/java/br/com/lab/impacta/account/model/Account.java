a8bb68f2b0cfa877e4a8bc3ea79f3c0b07cdc6e5
vinicius-schulz <vinicius.schulz.cntr@spassu.com.br>
2021-10-16 12:38:39 -0300

    criaçãa tabela account

--- account/src/main/java/br/com/lab/impacta/account/model/Account.java
+++ account/src/main/java/br/com/lab/impacta/account/model/Account.java
@@ -1,9 +1,12 @@
 package br.com.lab.impacta.account.model;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
@@ -14,5 +17,25 @@ public class Account {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	private Long number;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "person_id")
+	private Person person;
+
+	private Double balance;
+
+	public boolean debit(Double valueDebit) {
+		if (this.getBalance() < valueDebit) {
+			return false;
+		}
+
+		Double debitedAmount = this.getBalance() - valueDebit;
+		setBalance(debitedAmount);
+
+		return true;
+
+	}
 
 }
